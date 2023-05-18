8f8e2d44f449d77406f12fc84cf20f9a7d8c18c1
choudharyprince890 <choudharyprince890@gmail.com>
2023-05-19 01:25:18 +0530

    Updates

---
+++ precache-manifest.91d8c2b05728cb70bc219d6330da3db4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "744da152e9a520abe8dc",
+    "url": "/Prince-Portfolio/static/css/main.37267430.chunk.css"
+  },
+  {
+    "revision": "744da152e9a520abe8dc",
+    "url": "/Prince-Portfolio/static/js/main.744da152.chunk.js"
+  },
+  {
+    "revision": "96661330b616f1bfd583",
+    "url": "/Prince-Portfolio/static/css/1.05cfcdc6.chunk.css"
+  },
+  {
+    "revision": "96661330b616f1bfd583",
+    "url": "/Prince-Portfolio/static/js/1.96661330.chunk.js"
+  },
+  {
+    "revision": "cbfe3ac172a77259d23b",
+    "url": "/Prince-Portfolio/static/js/runtime~main.cbfe3ac1.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "/Prince-Portfolio/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "/Prince-Portfolio/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "/Prince-Portfolio/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "/Prince-Portfolio/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "/Prince-Portfolio/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "/Prince-Portfolio/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "68361e8bd3a1445b8fe94749d68955ac",
+    "url": "/Prince-Portfolio/index.html"
+  }
+];
